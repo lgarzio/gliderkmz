2df1ae6c05b3758ec163af8353a0ac19d0b5e711
Lori Garzio <lgarzio@marine.rutgers.edu>
2024-03-05 14:42:12 -0500

    update kmzs

--- gliderkmz.py
+++ gliderkmz.py
@@ -2,7 +2,7 @@
 
 """
 Author: lgarzio on 2/28/2024
-Last modified: lgarzio on 2/28/2024
+Last modified: lgarzio on 3/5/2024
 Test glider kmz generation
 """
 
@@ -18,34 +18,59 @@ def format_ts_str(timestamp):
     return dt.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M')
 
 
+def format_ts_epoch(timestamp):
+    return dt.datetime.fromtimestamp(timestamp, dt.UTC).strftime('%Y-%m-%d %H:%M')
+
+
 glider = 'ru40'
 savedir = '/Users/garzio/Documents/repo/lgarzio/gliderkmz/templates/'
+glider_tails = 'http://marine.rutgers.edu/~kerfoot/icons/glider_tails/'
+ts_now = dt.datetime.now(dt.UTC).strftime('%m/%d/%y %H:%M')
 
 # load the template
 environment = Environment(loader=FileSystemLoader(savedir))
 template = environment.get_template('template_ts.kml')
 track_template = environment.get_template('track_macro.kml')
+surfacing_template = environment.get_template('surface_event_macro.kml')
+text_box_template = environment.get_template('text_box_macro.kml')
 
 glider_api = 'https://marine.rutgers.edu/cool/data/gliders/api/'
 active_deployments = requests.get(f'{glider_api}deployments/?active').json()['data']
 for ad in active_deployments:
     glider_name = ad['glider_name']
     if glider_name == glider:
-        filename = f'{glider_name}-test.kml'
+        filename = f'{glider_name}-test2.kml'
         savefile = os.path.join(savedir, filename)
         deployment = ad['deployment_name']
+        glider_tail = os.path.join(glider_tails, f'{glider_name}.png')
 
         # last surfacing
-        last_surfacing = ad['last_surfacing']
-        ls_connect_ts = format_ts_str(last_surfacing['connect_ts'])
-        ls_disconnect_ts = format_ts_str(last_surfacing['disconnect_ts'])
-        ls_gps_connect_ts = dt.datetime.fromtimestamp(last_surfacing['connect_time_epoch'], dt.UTC).strftime('%Y-%m-%d %H:%M')
+        ls = ad['last_surfacing']
 
-        # track
+        ls_connect_ts = format_ts_str(ls['connect_ts'])
+        ls_disconnect_ts = format_ts_str(ls['disconnect_ts'])
+        ls_gps_connect_ts = format_ts_epoch(ls['connect_time_epoch'])
+
+        last_surfacing_dict = dict(
+            connect_ts=ls_connect_ts,
+            disconnect_ts=ls_disconnect_ts,
+            nmea_lat=ls['gps_lat'],
+            nmea_lon=ls['gps_lon'],
+            gps_connect_ts=ls_gps_connect_ts,
+            reason=ls['surface_reason'],
+            mission=ls['mission'],
+            filename=ls['filename'],
+            filename_8x3=ls['the8x3_filename'],
+            dsvr_log=ls['dsvr_log_name'],
+        )
+
+        # track information
+        # gather track timestamp and location from the API
         track_dict = dict(
             gps_epoch=np.array([], dtype='int'),
             lon=np.array([], dtype='float'),
-            lat=np.array([], dtype='float')
+            lat=np.array([], dtype='float'),
+            sid=np.array([], dtype='int')
         )
         track_features = requests.get(f'{glider_api}tracks/?deployment={deployment}').json()['features']
         for tf in track_features:
@@ -53,9 +78,14 @@ for ad in active_deployments:
                 track_dict['gps_epoch'] = np.append(track_dict['gps_epoch'], tf['properties']['gps_epoch'])
                 track_dict['lon'] = np.append(track_dict['lon'], tf['geometry']['coordinates'][0])
                 track_dict['lat'] = np.append(track_dict['lat'], tf['geometry']['coordinates'][1])
+                track_dict['sid'] = np.append(track_dict['sid'], tf['properties']['sid'])
         track_df = pd.DataFrame(track_dict)
         track_df.sort_values(by='gps_epoch', inplace=True, ignore_index=True)
 
+        # find the deployment id
+        deployment_sid = int(track_df.iloc[0]['sid'])
+
+        # build the dictionary that contains the track information to input into the kml template
         track_dict = dict()
         for idx, row in track_df.iterrows():
             if idx > 0:
@@ -71,19 +101,38 @@ for ad in active_deployments:
                     end_lat=row.lat
                 )
 
+        # surface events
+        surface_events = requests.get(f'{glider_api}surfacings/?deployment={deployment}').json()['data']
+
+        # find the deployment location surface record
+        for se in surface_events:
+            if se['surfacing_id'] == deployment_sid:
+                deploy_connect_ts = format_ts_epoch(se['connect_time_epoch'])
+                deploy_disconnect_ts = format_ts_epoch(se['disconnect_time_epoch'])
+                deploy_gps_connect_ts = format_ts_epoch(se['gps_timestamp_epoch'])
+
+                deployment_dict = dict(
+                    connect_ts=deploy_connect_ts,
+                    disconnect_ts=deploy_disconnect_ts,
+                    nmea_lat=se['gps_lat'],
+                    nmea_lon=se['gps_lon'],
+                    gps_connect_ts=deploy_gps_connect_ts,
+                    reason=se['surface_reason'],
+                    mission=se['mission'],
+                    filename=se['filename'],
+                    filename_8x3=se['the8x3_filename'],
+                    dsvr_log=se['dsvr_log_name'],
+                )
+
+        # render all of the information into the kml template
         content = template.render(
+            ts_now=ts_now,
             glider_name=glider_name,
-            ls_connect_ts=ls_connect_ts,
-            ls_disconnect_ts=ls_disconnect_ts,
-            ls_nmea_lat=last_surfacing['gps_lat'],
-            ls_nmea_lon=last_surfacing['gps_lon'],
-            ls_gps_connect_ts=ls_gps_connect_ts,
-            ls_reason=last_surfacing['surface_reason'],
-            ls_mission=last_surfacing['mission'],
-            ls_filename=last_surfacing['filename'],
-            ls_8x3_filename=last_surfacing['the8x3_filename'],
-            ls_dsvr_log=last_surfacing['dsvr_log_name'],
-            track_info=track_dict
+            glider_tail=glider_tail,
+            last_surfacing_info=last_surfacing_dict,
+            deployment_info=deployment_dict,
+            track_info=track_dict,
+            surface_event_info=surface_events
         )
 
         with open(savefile, mode="w", encoding="utf-8") as message:
