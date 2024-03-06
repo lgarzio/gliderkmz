6984d4e4850f61fdac082621bb9f33fa595c71dd
Lori Garzio <lgarzio@marine.rutgers.edu>
2024-03-05 21:15:01 -0500

    update kmzs

--- gliderkmz.py
+++ gliderkmz.py
@@ -11,11 +11,68 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 import requests
-from jinja2 import Environment, FileSystemLoader, meta
+from jinja2 import Environment, FileSystemLoader
+pd.set_option('display.width', 320, "display.max_columns", 10)
 
 
-def format_ts_str(timestamp):
-    return dt.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M')
+# def format_ts_str(timestamp):
+#     return dt.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M')
+
+def add_sensor_values(data_dict, sensor_name, sensor_api_data):
+    """
+    Add values from specific sensors to dictionary summaries
+    """
+    sensor_df = pd.DataFrame(sensor_api_data)
+    disconnect_ts_format = dt.datetime.strptime(data_dict['disconnect_ts'], '%Y-%m-%d %H:%M').strftime('%Y-%m-%dT%H:%M:00')
+    try:
+        sensor_value = sensor_df.loc[sensor_df.ts == disconnect_ts_format].value.values[0]
+    except IndexError:
+        sensor_value = None
+    data_dict[sensor_name] = sensor_value
+
+
+def build_deployment_lastsurf_dict(data):
+    """
+    Build the dictionaries for the data that populates the deployment and last surfacing locations in the kml
+    :param data: dictionary
+    """
+    connect_ts = format_ts_epoch(data['connect_time_epoch'])
+    disconnect_ts = format_ts_epoch(data['disconnect_time_epoch'])
+    gps_connect_ts = format_ts_epoch(data['gps_timestamp_epoch'])
+    try:
+        waypoint_range_km = data['waypoint_range_meters'] / 1000
+    except TypeError:
+        waypoint_range_km = None
+
+    return_dict = dict(
+        connect_ts=connect_ts,
+        disconnect_ts=disconnect_ts,
+        nmea_lat=data['gps_lat'],
+        nmea_lon=data['gps_lon'],
+        gps_connect_ts=gps_connect_ts,
+        reason=data['surface_reason'],
+        mission=data['mission'],
+        filename=data['filename'],
+        filename_8x3=data['the8x3_filename'],
+        dsvr_log=data['dsvr_log_name'],
+        segment_ewo=f"{data['segment_errors']}/{data['segment_warnings']}/{data['segment_oddities']}",
+        mission_ewo=f"{data['mission_errors']}/{data['mission_warnings']}/{data['mission_oddities']}",
+        total_ewo=f"{data['total_errors']}/{data['total_warnings']}/{data['total_oddities']}",
+        dive_time=None,  # minutes  # ***** need to find all of these in the API *****
+        dive_dist=None,  # km
+        total_speed=None,  # m/s
+        total_speed_bearing=None,
+        current_speed=None,  # m/s
+        current_speed_bearing=None,
+        glide_speed=None,  # m/s
+        glide_speed_bearing=None,
+        waypoint_lat=data['waypoint_lat'],
+        waypoint_lon=data['waypoint_lon'],
+        waypoint_range=waypoint_range_km,
+        waypoint_bearing=data['waypoint_bearing_degrees']
+    )
+
+    return return_dict
 
 
 def format_ts_epoch(timestamp):
@@ -23,6 +80,7 @@ def format_ts_epoch(timestamp):
 
 
 glider = 'ru40'
+sensor_list = ['m_battery', 'm_vacuum']
 savedir = '/Users/garzio/Documents/repo/lgarzio/gliderkmz/templates/'
 glider_tails = 'http://marine.rutgers.edu/~kerfoot/icons/glider_tails/'
 ts_now = dt.datetime.now(dt.UTC).strftime('%m/%d/%y %H:%M')
@@ -44,25 +102,13 @@ for ad in active_deployments:
         deployment = ad['deployment_name']
         glider_tail = os.path.join(glider_tails, f'{glider_name}.png')
 
-        # last surfacing
-        ls = ad['last_surfacing']
+        # build the dictionary for the last surfacing information
+        last_surfacing_dict = build_deployment_lastsurf_dict(ad['last_surfacing'])
 
-        ls_connect_ts = format_ts_str(ls['connect_ts'])
-        ls_disconnect_ts = format_ts_str(ls['disconnect_ts'])
-        ls_gps_connect_ts = format_ts_epoch(ls['connect_time_epoch'])
-
-        last_surfacing_dict = dict(
-            connect_ts=ls_connect_ts,
-            disconnect_ts=ls_disconnect_ts,
-            nmea_lat=ls['gps_lat'],
-            nmea_lon=ls['gps_lon'],
-            gps_connect_ts=ls_gps_connect_ts,
-            reason=ls['surface_reason'],
-            mission=ls['mission'],
-            filename=ls['filename'],
-            filename_8x3=ls['the8x3_filename'],
-            dsvr_log=ls['dsvr_log_name'],
-        )
+        # add values for battery and vacuum to the last surfacing information
+        for sensor in sensor_list:
+            sensor_api = requests.get(f'{glider_api}sensors/?deployment={deployment}&sensor={sensor}').json()['data']
+            add_sensor_values(last_surfacing_dict, sensor, sensor_api)
 
         # track information
         # gather track timestamp and location from the API
@@ -104,31 +150,26 @@ for ad in active_deployments:
         # surface events
         surface_events = requests.get(f'{glider_api}surfacings/?deployment={deployment}').json()['data']
 
-        # find the deployment location surface record
+        surface_df = pd.DataFrame(surface_events)
+
+        # find the deployment location surface record  ***** this doesn't match up with the current kmzs *****
         for se in surface_events:
             if se['surfacing_id'] == deployment_sid:
-                deploy_connect_ts = format_ts_epoch(se['connect_time_epoch'])
-                deploy_disconnect_ts = format_ts_epoch(se['disconnect_time_epoch'])
-                deploy_gps_connect_ts = format_ts_epoch(se['gps_timestamp_epoch'])
 
-                deployment_dict = dict(
-                    connect_ts=deploy_connect_ts,
-                    disconnect_ts=deploy_disconnect_ts,
-                    nmea_lat=se['gps_lat'],
-                    nmea_lon=se['gps_lon'],
-                    gps_connect_ts=deploy_gps_connect_ts,
-                    reason=se['surface_reason'],
-                    mission=se['mission'],
-                    filename=se['filename'],
-                    filename_8x3=se['the8x3_filename'],
-                    dsvr_log=se['dsvr_log_name'],
-                )
+                # build the dictionary for the deployment information
+                deployment_dict = build_deployment_lastsurf_dict(se)
+
+                # add values for battery and vacuum to deployment information
+                for sensor in sensor_list:
+                    sensor_api = requests.get(f'{glider_api}sensors/?deployment={deployment}&sensor={sensor}').json()['data']
+                    add_sensor_values(deployment_dict, sensor, sensor_api)
 
         # render all of the information into the kml template
         content = template.render(
             ts_now=ts_now,
             glider_name=glider_name,
             glider_tail=glider_tail,
+            ls_connect_ts=last_surfacing_dict['connect_ts'],
             last_surfacing_info=last_surfacing_dict,
             deployment_info=deployment_dict,
             track_info=track_dict,
